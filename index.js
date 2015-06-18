9dbd0211cd06300d820aa16634a4c555aca89c85
Juha Lindstedt <juha@pakastin.fi>
2015-06-18 12:59:57 +0300

    updated mongodb

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-
 'use strict'
+
+var MongoDB = require('mongodb')
 
 module.exports = OplogStream
 
 function OplogStream (callback) {
-  var MongoDB = require('mongodb')
   var oplogurl = 'mongodb://localhost/local'
 
   MongoDB.MongoClient.connect(oplogurl, function (err, db) {
     if (err) {
       return callback(err)
     }
-    db.collection("oplog.rs", function (err, oplog) {
+    db.collection('oplog.rs', function (err, oplog) {
       if (err) {
         return callback(err)
       }
       var now = Date.now()
-      var timestamp = MongoDB.Timestamp(0, (now/1000 | 0))
+      var timestamp = MongoDB.Timestamp(0, (now / 1000 | 0))
       var cursor = oplog.find({ts: {$gt: timestamp}}, {
         tailable: true,
         awaitdata: true,
