9ceb1659e8341ea9c1ba4cae396f87078c6dc780
Juha Lindstedt <juha@pakastin.fi>
2015-10-12 17:17:27 +0300

    updated mongodb

--- index.js
+++ index.js
@@ -11,25 +11,31 @@ function OplogStream (callback) {
     if (err) {
       return callback(err)
     }
+
     db.collection('oplog.rs', function (err, oplog) {
       if (err) {
         return callback(err)
       }
+
       var now = Date.now()
       var timestamp = MongoDB.Timestamp(0, (now / 1000 | 0))
+
       var cursor = oplog.find({ts: {$gt: timestamp}}, {
         tailable: true,
         awaitdata: true,
         oplogReplay: true,
         numberOfRetries: -1
       })
+
       var stream = cursor.stream()
       var destroy = stream.destroy
+
       stream.destroy = function () {
         cursor.close()
         destroy.call(stream)
         db.close()
       }
+
       callback(null, stream)
     })
   })
